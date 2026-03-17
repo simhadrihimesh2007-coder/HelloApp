c96632753777bf9111161507144620e7ee8213bb
simhadrihimesh2007-coder <simhadrihimesh2007@gmail.com>
2026-03-17 13:21:55 +0530

    uc2: to print Hello himesh

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,11 @@
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 
+        // Get the name from command-line argument
+        String name = args[0];
+
+        // Print greeting
+        System.out.println("Hello, " + name + "!");
+    }
 }
